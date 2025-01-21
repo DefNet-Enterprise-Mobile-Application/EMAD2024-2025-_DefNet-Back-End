caee7645b86cc8c9fadf2c6581b516d98464bd45
Umberto Della Monica <umberto.dellamonica@hotmail.it>
2025-01-21 02:38:05 +0100

    Update settings_service_controller.py

--- defnet_backend/app/controller/settings_service_controller.py
+++ defnet_backend/app/controller/settings_service_controller.py
@@ -5,7 +5,7 @@ from typing import Dict
 from requests import Session
 from controller.websocket_controller import manager
 from database.database import get_db
-
+import yaml
 
 router = APIRouter()
 
@@ -24,7 +24,7 @@ services_db: Dict[str, bool] = {
 }
 
 
-################################### IDS and IPS Service 
+################################### IDS and IPS Service #################################################
 
 
 # Funzione per avviare IDS-IPS
@@ -53,6 +53,37 @@ def stop_ids_ips():
         raise HTTPException(status_code=500, detail="Error stopping IDS-IPS")
 
 
+################################################## Parental Control Service ###################################################
+
+def toggle_parental_control(file_path, enable):
+    """
+    Modifica il valore di `parental_enabled` in un file YAML.
+
+    :param file_path: Percorso del file YAML.
+    :param enable: Booleano, True per abilitare il controllo parentale, False per disabilitarlo.
+    """
+    try:
+        # Legge il file YAML
+        with open(file_path, 'r') as file:
+            config = yaml.safe_load(file)
+        
+        # Modifica il valore di `parental_enabled`
+        if 'filtering' in config and isinstance(config['filtering'], dict):
+            config['filtering']['parental_enabled'] = enable
+        else:
+            print("Errore: Sezione 'filtering' non trovata o malformata.")
+            return
+
+        # Scrive il file YAML aggiornato
+        with open(file_path, 'w') as file:
+            yaml.dump(config, file, default_flow_style=False)
+        
+        print(f"Configurazione aggiornata: `parental_enabled` impostato a {enable}.")
+        
+        subprocess.run(['service', 'adguardhome','restart'], check=True)
+
+    except Exception as e:
+        print(f"Errore durante la modifica del file YAML: {e}")
 
 
 
@@ -80,6 +111,14 @@ async def toggle_service(service_name: str, service_request: ServiceRequest, db:
             stop_ids_ips()  # Ferma IDS-IPS
 
 
+    if service_name == "Parental Control":
+        if service_request.enabled:
+            # Enable service 
+            toggle_parental_control(file_path="/etc/adguardhome.yaml",enable=True)
+        else:
+            toggle_parental_control(file_path="/etc/adguardhome.yaml",enable=False)
+
+
      # Prepara i dati di alert per il broadcast
     alert_data = {
         "serviceName": service_name,
@@ -94,7 +133,7 @@ async def toggle_service(service_name: str, service_request: ServiceRequest, db:
     return {"message": f"Service '{service_name}' has been {'enabled' if service_request.enabled else 'disabled'} successfully."}
 
 
-
+# Recupera le informazioni in merito ai servizi che sono runnati sul router 
 @router.get("/services", response_model=Dict[str, bool])
 async def get_services_status():
     """
@@ -104,3 +143,4 @@ async def get_services_status():
         return services_db
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Errore nel recupero degli stati dei servizi: {e}")
+
